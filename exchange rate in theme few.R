5dd82a4b4eb2cd61f86fd9a09b29f0c93e6bf70a
leehygh <94373642+leehygh@users.noreply.github.com>
2024-03-08 20:58:50 +0800

    Add files via upload

---
+++ exchange rate in theme few.R
@@ -0,0 +1,10 @@
+library(tidyquant)
+ex_rate <- tq_get("DEXCHUS", get = "economic.data", from = "2019-07-01")
+
+library(ggplot2)
+library(ggthemes)
+ggplot(ex_rate, aes(x = date, y = price)) +
+  geom_line(color = "#0074D9", size = 1) +
+  labs(y = "RMB/USD", x = "date", title = "RMB/USD Exchange Rate",
+       subtitle = "Creator: @NiHao20010424", caption =  "Source: FRED") +
+  theme_few()
